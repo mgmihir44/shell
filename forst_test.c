fbd78c7a56070c6b3688a03acfd24aac1d911e9c
mgmihir44 <mgmihir44@gmail.com>
2018-06-20 18:29:35 -0400

    Added sleep to parent process

--- forst_test.c
+++ forst_test.c
@@ -15,6 +15,7 @@ int main(){
 	if(proc_id == 0){
 		printf("\n*** Child Process ***\nCurrent PID: %d and Child PID: %d\n", getpid(), proc_id);
 	}else{
+		sleep(1);		/* Sleep for 1 second */
 		printf("\n*** Parent Process ***\nCurrent PID: %d and Child PID: %d\n", getpid(), proc_id);
 	}
 
